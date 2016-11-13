d05c45ffa9ea2df3592cc02285f2f49d2998e491
Yury Noh <dliam123@naver.com>
2016-11-14 00:46:17 +0900

    UPDATE "SETUP JAVA"
    instantiated all the components in content view
    added events to all(op1~op4) buttons to do calculation(+, -, *, / in order)

--- app/src/main/java/com/example/yurynoh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yurynoh/myapplication/MainActivity.java
@@ -2,12 +2,71 @@ package com.example.yurynoh.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView result;
+    EditText num1, num2;
+    Button op1_plus, op2_minus, op3_multiply, op4_divide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // instantiate content components
+        num1 = (EditText)findViewById(R.id.num1);
+        num2 = (EditText)findViewById(R.id.num2);
+        op1_plus  = (Button)findViewById(R.id.op1_plus);
+        op2_minus = (Button)findViewById(R.id.op2_minus);
+        op3_multiply = (Button)findViewById(R.id.op3_multiply);
+        op4_divide = (Button)findViewById(R.id.op4_divide);
+        result = (TextView)findViewById(R.id.result);
+
+        // assign click events to buttons
+        op1_plus.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                double x = Double.parseDouble(num1.getText().toString());
+                double y = Double.parseDouble(num2.getText().toString());
+
+                result.setText(x + " + " + y + " = " + (double)(x+y));
+            }
+        });
+        op2_minus.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                double x = Double.parseDouble(num1.getText().toString());
+                double y = Double.parseDouble(num2.getText().toString());
+
+                result.setText(x + " - " + y + " = " + (double)(x-y));
+            }
+        });
+        op3_multiply.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                double x = Double.parseDouble(num1.getText().toString());
+                double y = Double.parseDouble(num2.getText().toString());
+
+                result.setText(x + " * " + y + " = " + (double)(x*y));
+            }
+        });
+        op4_divide.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                double x = Double.parseDouble(num1.getText().toString());
+                double y = Double.parseDouble(num2.getText().toString());
+
+                result.setText(x + " / " + y + " = " + (double)(x/y));
+            }
+        });
     }
 }
