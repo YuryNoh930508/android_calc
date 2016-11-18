42cd32b3bf0aa1b0cbd6d60818640ea008151267
Yury Noh <dliam123@naver.com>
2016-11-18 19:49:51 +0900

    UPDATE "class mandatory updates"
    1. toast when num1 is empty
    2. toast when num2 is empty
    3. toast when num2 is 0 when operator is division
    4. type 'double' calculation

--- README.md
+++ README.md
@@ -15,3 +15,9 @@
 
   <h3>WHEN! modulo</h3>
   <img src='/app/pics/Screenshot_1479432371.png?raw=true' width='200px'>
+  <h3>WHEN! num1 is empty</h3>
+  <img src='/app/pics/Screenshot_1479465809.png?raw=true' width='200px'>
+  <h3>WHEN! num2 is empty</h3>
+  <img src='/app/pics/Screenshot_1479465816.png?raw=true' width='200px'>
+  <h3>WHEN! num2 is 0 when operator is division</h3>
+  <img src='/app/pics/Screenshot_1479465825.png?raw=true' width='200px'>
--- app/src/main/java/com/example/yurynoh/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yurynoh/myapplication/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.yurynoh.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,56 +30,56 @@ public class MainActivity extends AppCompatActivity {
         op5_modulo = (Button)findViewById(R.id.op5_modulo);
         result = (TextView)findViewById(R.id.result);
 
+        View.OnClickListener clickButton = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String x_str = num1.getText().toString();
+                String y_str = num2.getText().toString();
+
+                if(x_str.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "num1 is empty", Toast.LENGTH_SHORT).show();
+                } else if(y_str.isEmpty()) {
+                    Toast.makeText(getApplicationContext(), "num2 is empty", Toast.LENGTH_SHORT).show();
+                } else {
+                    double x = Double.parseDouble(x_str);
+                    double y = Double.parseDouble(y_str);
+
+                    switch (v.getId()) {
+                        case R.id.op1_plus:
+                            result.setText(x + " + " + y + " = " + (double)(x+y));
+                            break;
+                        case R.id.op2_minus:
+                            result.setText(x + " - " + y + " = " + (double)(x-y));
+                            break;
+                        case R.id.op3_multiply:
+                            result.setText(x + " * " + y + " = " + (double)(x*y));
+                            break;
+                        case R.id.op4_divide:
+                            if(y_str.equals("0")) {
+                                Toast.makeText(getApplicationContext(), "num1 can not be divided by 0", Toast.LENGTH_SHORT).show();
+                            } else {
+                                result.setText(x + " / " + y + " = " + (double) (x / y));
+                            }
+                            break;
+                        case R.id.op5_modulo:
+                            if(y_str.equals("0")) {
+                                Toast.makeText(getApplicationContext(), "num1's remainder does not exist if num2 is 0", Toast.LENGTH_SHORT).show();
+                            } else {
+                                result.setText(x + " % " + y + " = " + (double) (x % y));
+                            }
+                            break;
+                    }
+                }
+            }
+
+        };
+
         // assign click events to buttons
-        op1_plus.setOnClickListener(new View.OnClickListener() {
+        Button[] operator = new Button[]{op1_plus, op2_minus, op3_multiply, op4_divide, op5_modulo};
 
-            @Override
-            public void onClick(View v) {
-                double x = Double.parseDouble(num1.getText().toString());
-                double y = Double.parseDouble(num2.getText().toString());
-
-                result.setText(x + " + " + y + " = " + (double)(x+y));
-            }
-        });
-        op2_minus.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                double x = Double.parseDouble(num1.getText().toString());
-                double y = Double.parseDouble(num2.getText().toString());
-
-                result.setText(x + " - " + y + " = " + (double)(x-y));
-            }
-        });
-        op3_multiply.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                double x = Double.parseDouble(num1.getText().toString());
-                double y = Double.parseDouble(num2.getText().toString());
-
-                result.setText(x + " * " + y + " = " + (double)(x*y));
-            }
-        });
-        op4_divide.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                double x = Double.parseDouble(num1.getText().toString());
-                double y = Double.parseDouble(num2.getText().toString());
-
-                result.setText(x + " / " + y + " = " + (double)(x/y));
-            }
-        });
-        op5_modulo.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                double x = Double.parseDouble(num1.getText().toString());
-                double y = Double.parseDouble(num2.getText().toString());
-
-                result.setText(x + " % " + y + " = " + (double)(x%y));
-            }
-        });
+        for(int i=0;i<operator.length;i++) {
+            operator[i].setOnClickListener(clickButton);
+        }
     }
 }
